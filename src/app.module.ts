c7a731e6956e2355756c70f8008f99fe893f357e
chaifangbo <chaifangbo@zuoyebang.com>
2020-03-24 12:15:38 +0800

    静态服务

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,6 @@
-import {
-  Module,
-  NestModule,
-  MiddlewareConsumer,
-  RequestMethod,
-} from '@nestjs/common';
+import { Module } from '@nestjs/common';
+import { join } from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ArticalsModule } from './modules/articals/articals.module';
 import { UsersModule } from './modules/users/users.module';
@@ -11,6 +8,9 @@ import { AppController } from './app.controller';
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/nest'),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../home-fe/dist'),
+    }),
     ArticalsModule,
     UsersModule,
   ],
