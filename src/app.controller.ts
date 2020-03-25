96f6635f8adcb566f4e2d6df37ab3b070268b8d4
chaifangbo <chaifangbo@zuoyebang.com>
2020-03-25 13:55:15 +0800

    action

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,6 +4,6 @@ import { Controller, Get } from '@nestjs/common';
 export class AppController {
   @Get()
   getHello(): string {
-    return 'hello world';
+    return 'hello world, nice day';
   }
 }
